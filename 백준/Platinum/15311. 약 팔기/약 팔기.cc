7166fdb2d14c710893b58e82117b6a12a9b5e150
ffvv0123 <63843822+ffvv0123@users.noreply.github.com>
2023-10-06 20:48:20 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+
+	cout << 2000 << endl;	
+
+	for(int i = 0; i <1000; i++) {
+
+		cout << 1 << " ";
+
+	}
+
+	
+
+	for(int i = 0; i <1000; i++) {
+
+		cout << 1000 << " ";
+
+	}
+
+}
+
+	    
+
+	    
