f7d08971e0b94ca36b285e8a7bab0c7259b1e83d
ffvv0123 <63843822+ffvv0123@users.noreply.github.com>
2024-04-08 10:19:38 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,8 +1,9 @@
 while True:
-    num = input()
-    if num == '0':
+    arr = input()
+    if arr == "0":
         break
-    if num == num[::-1]: #이 부분은 입력받은 수를 뒤집어 앞의 숫자와 비교해주는 슬라이스 함수에 대해 몰랐지만 검색하여 활용하게 되었습니다
+
+    if arr == arr[::-1]:
         print("yes")
     else:
         print("no")
