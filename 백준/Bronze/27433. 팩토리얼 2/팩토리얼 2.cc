040ca537012b65e91241183167fff093a18e5c86
ffvv0123 <63843822+ffvv0123@users.noreply.github.com>
2024-05-02 14:41:33 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+long factorial(int n) {
+    if(n <= 1){
+        return 1;
+    }
+    else return n * factorial(n-1);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << factorial(n);
+}
