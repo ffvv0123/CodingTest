383876293c8a3cc5fcba168e59a481f71655eb99
Suhyeon Jo <63843822+ffvv0123@users.noreply.github.com>
2024-07-17 05:29:11 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<int> answer;
+
+void dfs(vector<int> &answer, int start) {
+    if (answer.size() == M) {
+        for(int num : answer)
+            cout << num << " ";
+        cout << '\n';
+        return;
+    }
+
+    for(int i = start; i < N+1; i++) {
+        answer.push_back(i);
+        dfs(answer, i);
+        answer.pop_back();
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> N >> M;
+    dfs(answer, 1);
+
+    return 0;
+}
