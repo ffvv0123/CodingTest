df13c6deba86b87c235e0be06198c4985dbf6e07
Suhyeon Jo <63843822+ffvv0123@users.noreply.github.com>
2024-07-17 00:59:25 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N, M, a, b;
+    int answer = 0;
+    int list[100002];
+    int dp[100002];
+
+    cin >> N >> M;    
+
+    for(int i=0; i<N; i++)
+        cin >> list[i];
+
+    dp[0] = list[0];    
+    for(int i = 1; i < N; i++)
+        dp[i] = dp[i-1] + list[i];
+
+    for(int i = 0; i < M; i++) {
+        cin >> a >> b;
+        a--; b--;
+        
+        if (a == 0) cout << dp[b] << '\n';
+        else if (a == b) cout << dp[b] - dp[b-1] << '\n';
+        else cout << dp[b] - dp[a-1] << '\n';
+    }
+
+    return 0;
+}
