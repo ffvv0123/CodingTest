75fcd3a8d7a54d1a06fe88e84bc395c5c687050b
Suhyeon Jo <63843822+ffvv0123@users.noreply.github.com>
2024-08-03 16:31:36 +0900

    [Unrated] Title: 별 찍기 - 14, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/2556. 별 찍기 － 14/별 찍기 － 14.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main() {
+    cin >> N;
+    
+    for(int i=0; i < N; i++) {
+        for(int j=0; j < N; j++)
+            cout << "*";
+        cout << '\n';
+    }
+    
+    return 0;
+}
