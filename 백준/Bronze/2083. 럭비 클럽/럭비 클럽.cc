6a1990ce906ef385c556873ffba2ac13a7eb994d
Suhyeon Jo <63843822+ffvv0123@users.noreply.github.com>
2024-08-23 08:36:20 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string name;
+    int age, weight;
+
+    while(true) {
+        cin >> name >> age >> weight;
+        if(name == "#" && age == 0 && weight == 0) break;
+
+        cout << name << " ";
+        if (age > 17 || weight >= 80) cout << "Senior\n";
+        else cout << "Junior\n";
+    }
+
+    return 0;
+}
