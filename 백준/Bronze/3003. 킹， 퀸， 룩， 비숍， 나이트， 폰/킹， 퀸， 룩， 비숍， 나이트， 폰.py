1c529e43577fbf723f28cbb8ebd2764d0328cf68
Suhyeon Jo <63843822+ffvv0123@users.noreply.github.com>
2024-08-30 21:45:00 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,6 +1,5 @@
-import sys
-chess = list(map(int, sys.stdin.readline().split()))
-ans = [1,1,2,2,2,8]
+chess = list(map(int, input().split()))
+answer = [1, 1, 2, 2, 2, 8]
 
-for i, j in zip(chess, ans):
-    print(j - i, end=' ')
+for i, j in zip(chess, answer):
+    print(j - i, end = " ")
